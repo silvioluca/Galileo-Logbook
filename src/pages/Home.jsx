13a79335271a20402d45444d60313ab0eb60c98a
silvioluca <34754095+silvioluca@users.noreply.github.com>
2026-09-02 17:18:24 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
       <section className="hero-full">
         <img
           className="hero-bg-image"
-          src="/images/galileo-portrait.png"
+          src="./images/galileo-portrait.png"
           alt=""
           aria-hidden="true"
         />
